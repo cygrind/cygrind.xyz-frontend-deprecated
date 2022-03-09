47a45a9b80c1b84b29a43ab2b5ff096fdc37f6b0
krista-chan <qbotdev84@gmail.com>
2022-03-09 18:58:09 +0000

    what the fuck am i doinggggggggggggg

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 const express = require("express");
 const { join } = require("path");
 const app = express();
-const port = 8080;
-let { connect, connection, set, Promise: MongoosePromise } = require("mongoose");
-
-// mongoose
-set("useFindAndModify", false);
-set("useCreateIndex", true);
-MongoosePromise = global.Promise;
-
-const dbOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  poolSize: 5,
-  connectTimeoutMS: 10000,
-  family: 4
-};
-
-// await connect("mongodb://127.0.0.1:6969/cygrind", dbOptions);
-// 
-// connection.on("connected", () => {
-  // console.log("Connected to database");
-// });
+const port = 3000;
 
 // middleware
 app.use(express.urlencoded({extended: true}));
--- routes/get.js
+++ routes/get.js
@@ -1,10 +1,46 @@
 const router = require("express").Router();
+const aero = require("@aero/http");
 
 module.exports = {
-  home: router.get("/", (req, res) => {
+  home: router.get("/", (_, res) => {
     res.render("index.ejs");
   }),
-  florp: router.get("/florp", (req, res) => {
+  florp: router.get("/florp", (_, res) => {
     res.render("florp.ejs");
+  }),
+  dashboardGet: router.get("/dashboard", (_, res) => {
+    res.render("dashboard.ejs");
+  }),
+  loginPost: router.post("/login", async (req, res) => {
+    let json = JSON.stringify(req.body);
+
+    let apiRes = await aero("http://127.0.0.1:8080/login?t=standard").method("POST").header({ "Content-Type": "application/json" }).body(json).send();
+
+    if (apiRes.statusCode !== 200) {
+      res.redirect("/");
+    }
+
+    let body = await apiRes.body.json();
+    console.log(body);
+
+    res
+      .cookie("session", body.session_id)
+      .cookie("username", body.username)
+      .cookie("displayname", body.displayname)
+      .cookie("user_id", body.user_id)
+      .redirect("/dashboard")
+  }),
+  loginPost: router.post("/signup", async (req, res) => {
+    let json = JSON.stringify(req.body);
+
+    let apiRes = await aero("http://127.0.0.1:8080/signup?t=standard").method("POST").header({ "Content-Type": "application/json" }).body(json).send();
+    let body = await apiRes.body.json();
+
+    res
+      .cookie("session", body.session_id)
+      .cookie("username", body.username)
+      .cookie("displayname", body.displayname)
+      .cookie("user_id", body.user_id)
+      .redirect("/dashboard")
   }),
 };
