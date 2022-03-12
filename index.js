5986d6579fee08bd20247f2b5b0dd1040afcde15
krista-chan <qbotdev84@gmail.com>
2022-03-12 12:55:41 +0000

    Refactor anything + add auth

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { join } = require("path");
+const dotenv = require("dotenv");
 const app = express();
 const port = 3000;
+
+dotenv.config();
 
 // middleware
 app.use(express.urlencoded({extended: true}));
 app.set("views", join(__dirname, "views"));
 app.use(express.static("public"));
+app.use(express.json());
 
 // routing
 const get = require("./routes/get");
--- routes/get.js
+++ routes/get.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 const aero = require("@aero/http");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+
+const privKey = fs.readFileSync("private.pem");
 
 module.exports = {
   home: router.get("/", (_, res) => {
@@ -8,38 +12,70 @@ module.exports = {
   florp: router.get("/florp", (_, res) => {
     res.render("florp.ejs");
   }),
-  dashboardGet: router.get("/dashboard", (_, res) => {
+  dashboardGet: router.get("/dashboard", (req, res) => {
     res.render("dashboard.ejs");
   }),
   loginPost: router.post("/login", async (req, res) => {
-    let json = JSON.stringify(req.body);
+    let [status, body] = await apiReq(req, "login", "?t=standard", true);
 
-    let apiRes = await aero("http://127.0.0.1:8080/login?t=standard").method("POST").header({ "Content-Type": "application/json" }).body(json).send();
-    let body = await apiRes.body.json();
-    console.log(body);
-
-    // res
-    //   .cookie("session", body.session_id)
-    //   .cookie("username", body.username)
-    //   .cookie("displayname", body.displayname)
-    //   .cookie("user_id", body.user_id)
-    //   .redirect("/dashboard")
-
-    res.json(body);
+    res.status(status).json(body);
   }),
-  loginPost: router.post("/signup", async (req, res) => {
-    let json = JSON.stringify(req.body);
+  signupPost: router.post("/signup", async (req, res) => {
+    let errors = validateUsername(req.body.username);
 
-    let apiRes = await aero("http://127.0.0.1:8080/signup?t=standard").method("POST").header({ "Content-Type": "application/json" }).body(json).send();
-    let body = await apiRes.body.json();
+    if (errors.length > 0) {
+      let json = { errors: errors };
+      res.status(400).json(json);
 
-    // res
-    //   .cookie("session", body.session_id)
-    //   .cookie("username", body.username)
-    //   .cookie("displayname", body.displayname)
-    //   .cookie("user_id", body.user_id)
-    //   .redirect("/dashboard")
+      return;
+    }
 
-    res.json(body);
+    let [status, body] = await apiReq(req, "signup", "?t=standard", true);
+    res.status(status).json(body);
+  }),
+  logoutPost: router.post("/logout", async (req, res) => {
+    let [status, body] = await apiReq(req, "logout", null, false);
+
+    res.status(status).json(body);
   }),
 };
+
+function validateUsername(username) {
+  let errors = [];
+  let username_regex = /^[a-zA-Z0-9_ ]+$/g;
+
+  if (username.length < 3) {
+    errors.push("Your username is too short.");
+  }
+
+  if (!username_regex.test(username)) {
+    errors.push("Your username contains invalid characters.")
+  }
+
+  return errors;
+}
+
+function genToken(username, password) {
+  return jwt.sign({ username: username, password: password, exp: Date.now() }, privKey, { algorithm: "RS256" });
+}
+
+async function apiReq(req, endpoint, query, private) {
+  let headers;
+  if (private) {
+    headers = { "Content-Type": "application/json", "Authorization": "Bearer " + process.env.CYGRIND_API_TOKEN };
+  } else {
+    headers = { "Content-Type": "application/json" };
+  }
+
+  let json = JSON.stringify(req.body);
+
+  let apiRes = await aero(`http://127.0.0.1:8080/${endpoint}${query ? query : ""}`)
+    .method("POST")
+    .header(headers)
+    .body(json)
+    .send();
+
+  let body = await apiRes.body.json();
+
+  return [apiRes.statusCode, body];
+}
